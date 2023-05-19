39886b9141821c656dfd418a2b3a3ef4ea652280
Aam Hermansyah <74630284+AamHermansyah@users.noreply.github.com>
2023-05-20 06:49:03 +0700

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,7 +4,7 @@ import styles from '../styles';
 const Stats = () => (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb:6`}>
         {stats.map(stat => (
-            <div className={`flex-1 ${styles.flexCenter} flex-row mr-3`} key={stat.id}>
+            <div className={`flex-1 ${styles.flexCenter} flex-col sm:flex-row mr-3`} key={stat.id}>
                 <h4 className="font-poppins font-semibold text-[30px] xs:text-[40px] leading-[43px] xs:leading-[53px] text-white" >{stat.value}</h4>
                 <p className='font-poppins font-normal text-[20px] xs:text-[15px] leading-[21px] xs:leading-[26px] text-gradient uppercase ml-3'>{stat.title}</p>
             </div>
